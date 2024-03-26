01f1a73046366d38ddea9de0efe2951d95f60637
Panda <sherpand@gmail.com>
2024-03-25 21:02:01 -0300

    update test

--- src/config/passport-apple.ts
+++ src/config/passport-apple.ts
@@ -19,7 +19,7 @@ export function configurePassportApple(passport: any) {
       },
       async (req, accessToken, refreshToken, idToken, profile, done) => {
 				
-				// console.log(profile);
+				console.log(req.body.user);
 				// console.log(accessToken);
 				// console.log(refreshToken);
 
